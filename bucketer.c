2771d567c9fc22d50639f66e9d369a86be0d45a1
vaishnavi032001 <120644850+vaishnavi032001@users.noreply.github.com>
2022-12-15 16:34:40 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -17,7 +17,7 @@ void testBucketingByNumberOfCycles() {
   const int numberOfBatteries = sizeof(chargeCycleCounts) / sizeof(chargeCycleCounts[0]);
   printf("Counting batteries by usage cycles...\n");
   struct CountsByUsage counts = countBatteriesByUsage(chargeCycleCounts, numberOfBatteries);
-  assert(counts.lowCount == 2);
+  assert(counts.lowCount == 300);
   assert(counts.mediumCount == 3);
   assert(counts.highCount == 1);
   printf("Done counting :)\n");
